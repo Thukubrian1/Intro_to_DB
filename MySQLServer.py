197a673854c2a91fc998fa6d14be46ab6f43e393
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-10-23 23:07:17 +0300

    mysql

---
+++ MySQLServer.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection  = mysql.connector.connect(
+            host = "localhost",
+            user = "root",
+            password = "Thukubrian1@2024",
+)
+    
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+            print("Database 'alx_book_store' has successfully been created")
+
+    
+    except Error as e:
+        print("Error while connecting to MYSQL {e}")
+
+
+    finally:
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+        
+        print("MYSQL connection was closed")
+
+if __name__ == "__main__":
+    create_database()
