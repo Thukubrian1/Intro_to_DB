39f63581d8b168eae5e6ff9bece37b495e181754
Thukubrian1 <thukubrianngugi1@gmail.com>
2024-10-23 19:33:10 +0300

    my sql

---
+++ mysql/alx_book_store.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE alx_be_store;
+USE alx_be_store;
+
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE books (
+    book_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    author_id INT NOt NULL,
+    title VARCHAR(130),
+    price DOUBLE,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES authors (author_id)
+);
+
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(130) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT, 
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+
+CREATE TABLE order_details (
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,  
+    quantity DOUBLE,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
